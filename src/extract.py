87d654ae2181aa83a4ebd2360a92fff9dab289de
isabelleferreira <isabelleferreiraa999@gmail.com>
2026-06-26 22:15:01 -0300

    feat:  finalizando pipeline ETL com logger e main estruturado

--- src/extract.py
+++ src/extract.py
@@ -1,24 +1,7 @@
 import requests
-import pandas as pd
-import os
-import os
 
-os.makedirs("data", exist_ok=True)
-
-url = "https://fakestoreapi.com/products"
-
-response = requests.get(url)
-data = response.json()
-
-df = pd.DataFrame(data)
-
-# quebrar o campo "rating"
-rating_df = pd.json_normalize(df["rating"])
-
-# juntar de volta no dataframe principal
-df = df.drop(columns=["rating"]).join(rating_df)
-
-print(df.head())
-
-# SALVANDO O CSV (AQUI NO FINAL)
-df.to_csv(os.path.join("data", "produtos_tratados.csv"), index=False)
+def extract_data():
+    url = "https://fakestoreapi.com/products"
+    response = requests.get(url)
+    data = response.json()
+    return data
--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 from extract import extract_data
 from transform import transform_data
 from load import load_data
+from logger import setup_logger
+
+logger = setup_logger()
 
 def main():
+    logger.info("Iniciando ETL...")
+
+    logger.info("Extraindo dados da API...")
     data = extract_data()
+
+    logger.info("Transformando dados...")
     df = transform_data(data)
+
+    logger.info("Salvando dados...")
     load_data(df)
 
-    print("ETL finalizado com sucesso!")
+    logger.info("ETL finalizado com sucesso!")
 
 if __name__ == "__main__":
     main()
