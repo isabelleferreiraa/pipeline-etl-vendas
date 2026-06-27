58d24853b3d4ab0dcd285f15b5c37187e93b38cf
isabelleferreira <isabelleferreiraa999@gmail.com>
2026-06-27 16:44:08 -0300

    refactor: backup antes de reorganizar o projeto

---
+++ src/config.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
+
+PROCESSED_DATA_PATH = "data/processed/produtos_tratados.csv"
+API_URL = "https://fakestoreapi.com/products"
--- src/load.py
+++ src/load.py
@@ -1,6 +1,73 @@
 import os
+import psycopg2
+
+from logger import setup_logger
+from config import (
+    DB_HOST,
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    DB_PORT,
+    PROCESSED_DATA_PATH
+)
+
+logger = setup_logger()
+
 
 def load_data(df):
-    os.makedirs("data/processed", exist_ok=True)
+    """Salva os dados tratados em CSV e no PostgreSQL."""
 
-    df.to_csv("data/processed/produtos_tratados.csv", index=False)
+    # =========================
+    # 1. Salva o CSV
+    # =========================
+    os.makedirs(os.path.dirname(PROCESSED_DATA_PATH), exist_ok=True)
+    df.to_csv(PROCESSED_DATA_PATH, index=False)
+
+    conn = None
+    cur = None
+
+    try:
+        # =========================
+        # 2. Conecta ao PostgreSQL
+        # =========================
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            port=DB_PORT
+        )
+
+        cur = conn.cursor()
+
+        # =========================
+        # 3. Insere os dados
+        # =========================
+        for _, row in df.iterrows():
+            cur.execute(
+                """
+                INSERT INTO produtos (id, title, price, category)
+                VALUES (%s, %s, %s, %s)
+                ON CONFLICT (id) DO NOTHING;
+                """,
+                (
+                    int(row["id"]),
+                    row["title"],
+                    float(row["price"]),
+                    row["category"],
+                ),
+            )
+
+        conn.commit()
+        logger.info("Dados carregados no PostgreSQL com sucesso!")
+
+    except Exception as e:
+        logger.exception(f"Erro ao carregar dados no PostgreSQL: {e}")
+        raise
+
+    finally:
+        if cur:
+            cur.close()
+
+        if conn:
+            conn.close()
